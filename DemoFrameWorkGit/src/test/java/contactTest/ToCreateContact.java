dc274961e93dd2b5714fa3b52a5dcc1e41ecee2d
atris <atris@DESKTOP-6NUV340.mshome.net>
2024-10-04 13:40:18 +0530

    pushing

---
+++ DemoFrameWorkGit/src/test/java/contactTest/ToCreateContact.java
@@ -0,0 +1,14 @@
+package contactTest;
+
+public class ToCreateContact {
+	
+public static void main(String[] args) {
+	
+	System.out.println("Contact created successfully");
+	
+		
+
+	}
+
+
+}
---
+++ DemoFrameWorkGit/src/test/java/organizationTest/ToCreateOrganization.java
@@ -0,0 +1,11 @@
+package organizationTest;
+
+public class ToCreateOrganization {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Organization created successfully");
+
+	}
+
+}
